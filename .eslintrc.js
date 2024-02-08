f8a324155ef43d1db3d3c48decce71cbbe0d7dd2
csehviktor <csehviktor05@gmail.com>
2024-02-08 10:26:44 +0100

    add eslint package

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: true
+    },
+    plugins: ["@typescript-eslint"],
+    extends: [
+        "plugin:@next/next/recommended",
+        "plugin:@typescript-eslint/recommended-type-checked",
+        "plugin:@typescript-eslint/stylistic-type-checked"
+    ],
+    ignorePatterns: [".eslintrc.js"]
+    rules: {
+        "@typescript@typescript-eslint/array-type": "off",
+        "@typescript-eslint/consistent-type-definitions": "off",
+        "@typescript-eslint/consistent-type-imports": [
+            "warn", {
+                prefer: "type-imports",
+                fixStyle: "inline-type-imports"
+            }
+        ],
+        "@typescript-eslint/no-unused-vars": [
+            "warn", {
+                argsIgnorePattern: "^_"
+            }
+        ],
+        "@typescript-eslint/require-await": "off",
+        "@typescript-eslint/no-misused-promises": [
+            "error", {
+                checksVoidReturn: { attributes: false }
+            }
+        ]
+    }
+}
