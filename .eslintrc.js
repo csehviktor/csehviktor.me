82448c4ca26f46dc7aa934169d22c0e7f811dab7
csehviktor <csehviktor05@gmail.com>
2024-02-08 10:31:04 +0100

    fix config syntax

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
         "plugin:@typescript-eslint/recommended-type-checked",
         "plugin:@typescript-eslint/stylistic-type-checked"
     ],
-    ignorePatterns: [".eslintrc.js"]
+    ignorePatterns: [".eslintrc.js"],
     rules: {
         "@typescript@typescript-eslint/array-type": "off",
         "@typescript-eslint/consistent-type-definitions": "off",
