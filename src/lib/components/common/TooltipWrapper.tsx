a3cdc8696a8257579c5a3d426ff2573b9432a311
csehviktor <csehviktor05@gmail.com>
2024-03-15 02:53:14 +0100

    remove redundant components

--- src/lib/components/common/TooltipWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import type { Children } from '@/lib/models/components'
-
-export const TooltipWrapper = ({ children }: Children) => {
-    return(
-        <div className="group relative flex justify-center">
-            { children }
-        </div>
-    )
-}
--- src/lib/components/profile/Avatar.tsx
+++
@@ -1,9 +0,0 @@
-export const Avatar = ({ avatar }: { avatar: string }) => {
-    return(
-        <div className="md:w-64 md:h-64 w-48 h-48 rounded-3xl bg-center bg-cover"
-            style={{
-                backgroundImage: `url(${avatar})`
-            }}
-        />
-    )
-}
