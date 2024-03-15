ccc0f916791195abaeb669ce0b8a2854559db69e
csehviktor <csehviktor05@gmail.com>
2024-03-15 03:08:51 +0100

    code cleaning

--- src/lib/components/Card.tsx
+++ src/lib/components/Card.tsx
@@ -5,7 +5,7 @@ export const Card = ({ children, color }: CardModel) => {
         <div className='w-full flex-grow rounded-xl p-5 transition-all duration-300 bg-opacity-50 hover:bg-opacity-50 hover:scale-105 bg-[#050505] hover:bg-[#080808]'
             style={{
                 borderColor: color,
-                borderRightWidth: color ? '.3rem': '0',
+                borderRightWidth: color ? '.3rem' : '0',
             }}
         >
             { children }
