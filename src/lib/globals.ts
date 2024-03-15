290147758b0d318424512370d4984d8a8df3f8a6
csehviktor <csehviktor05@gmail.com>
2024-03-15 02:58:05 +0100

    update page data

--- src/lib/globals.ts
+++ src/lib/globals.ts
@@ -1,15 +1,19 @@
-import type { CategoryProps, GlobalProps, IconProps, SiteProps } from '@/lib/models/props'
-import { Socials } from '@/lib/models/variants'
+import type { BaseSection, HeaderSection, HeaderSection, SiteProp } from '@/lib/models/props'
+import { TbBrandTypescript, TbBrandKotlin, TbBrandNextjs, TbBrandVue, TbBrandTailwind, TbBrandMongodb } from 'react-icons/tb'
+import { FaSpotify, FaGithub, FaInstagram } from 'react-icons/fa'
+import { RiJavascriptLine } from 'react-icons/ri'
+import { SiPostgresql } from 'react-icons/si'
+import { LiaJava } from 'react-icons/lia'
 import { calculateAge } from '@/lib/utils'
-import { FaCode, FaGithub, FaHome, FaInstagram, FaSpotify, FaStar } from 'react-icons/fa'
 
-export const siteProperties: SiteProps = {
+
+export const siteProperties: SiteProp = {
     metadata: {
         metadataBase: new URL('https://csehviktor.me'),
         title: 'csehviktor.me',
         description: `A(n) ${ calculateAge() }-year-old university student from Hungary`,
         openGraph: {
-            images: '/og_image.jpg',
+            images: './og_image.jpg',
             title: 'csehviktor.me',
             url: 'https://csehviktor.me',
         }
@@ -19,92 +23,86 @@ export const siteProperties: SiteProps = {
     }
 }
 
-export const globalData: GlobalProps = {
+export const headerData: HeaderSection = {
     avatar: './avatar.jpg',
     name: 'Cseh Viktor',
     socials: [
         {
-            id: 0,
-            type: Socials.GITHUB,
             url: 'https://github.com/csehviktor',
             icon: {
-                type: FaGithub,
+                icon: FaGithub,
                 color: '#696969'
             }
         },
         {
-            id: 1,
-            type: Socials.INSTAGRAM,
             url: 'https://instagram.com/vehcsiktor',
             icon: {
-                type: FaInstagram,
+                icon: FaInstagram,
                 color: '#a31049',
             }
         },
         {
-            id: 2,
-            type: Socials.SPOTIFY,
             url: 'https://open.spotify.com/user/qjv1p5h89mnt8ch9c1hdmz1i5',
             icon: {
-                type: FaSpotify,
+                icon: FaSpotify,
                 color: '#095e05',
             }
-        },
+        }
     ]
 }
 
-export const descriptionData: CategoryProps[] = [
+export const baseData: BaseSection[] = [
     {
-        id: 0,
-        label: 'About Me 👋',
+        title: 'skills',
         description: [
-            `A(n) <strong>${ calculateAge() }</strong>-year-old university student from Hungary, whose main source of entertainment comes from web development and bug hunting.`,
-            'Able to use certain web frameworks prominently, such as Nextjs or Remix, along with languages like TypeScript/JavaScript, Vue, Java, Kotlin, C#.',
+            { icon: TbBrandTypescript, color: '#3178c6' },
+            { icon: RiJavascriptLine, color: '#f1e05a' },
+            { icon: TbBrandNextjs, color: '#ffffff' },
+            { icon: TbBrandVue, color: '#42b883' },
+            { icon: TbBrandKotlin, color: '#b124ea' },
+            { icon: LiaJava, color: '#b07219' },
+            { icon: TbBrandTailwind, color: '#38bdf8' },
+            { icon: TbBrandMongodb, color: '#00ed64' },
+            { icon: SiPostgresql, color: '#0064a5' }
         ]
     },
     {
-        id: 1,
-        label: 'Studies 📚',
-        list: [
+        title: 'languages',
+        card: [
             {
-                id: 0,
-                title: 'Jedlik Ányos Secondary School',
-                subtitle: '(2019-2023)',
-                description: [
-                    '<strong>Informatics Class - Advanced IT education</strong>',
-                    'A substantial portion of the knowledge forming the foundation of my "achievements" in IT is related to this place.'
-                ]
+                icon: {
+                    icon: './icons/flag_hu.svg',
+                },
+                title: 'Hungarian',
+                description: 'mother tongue'
             },
             {
-                id: 1,
+                icon: {
+                    icon: './icons/flag_gb.svg',
+                },
+                title: 'English',
+                description: 'fluent - C1 level (CEFR)'
+            },
+            {
+                icon: {
+                    icon: './icons/flag_it.svg',
+                },
+                title: 'Italian',
+                description: 'beginner - B1 level'
+            }
+        ]
+    },
+    {
+        title: 'studies',
+        card: [
+            {
                 title: 'Corvinus University of Budapest',
-                subtitle: '(2023-)',
-                description: [
-                    '<strong>Business Administration and Management</strong>',
-                    'Due to the extreme valuableness of combining IT and economics, I\'m pursuing my university studies in the field of economics.'
-                ]
+                description: 'BSc Business Administration and Management'
             },
+            {
+                title: 'Jedlik Ányos Secondary School',
+                description: 'Informatics Class'
+            }
         ]
-    }
-]
-
-export const navbarItems: IconProps[] = [
-    {
-        type: FaHome,
-        color: '#0a8f3b',
-        path: '/',
-        label: 'Home', 
-    },
-    {
-        type: FaStar,
-        color: '#c29810',
-        path: '/projects',
-        label: 'Projects', 
-    },
-    {
-        type: FaCode,
-        color: '#4f81fe',
-        path: 'https://github.com/csehviktor/csehviktor.me',
-        label: 'Source code'
     }
 ]
