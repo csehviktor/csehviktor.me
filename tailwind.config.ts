5885a3a84e880c98473492239637953b1e43a480
csehviktor <csehviktor05@gmail.com>
2024-03-15 03:09:46 +0100

    register plugin properly

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   plugins: [
-    'tailwindcss-animation-delay'
+    require('tailwindcss-animation-delay')
   ],
 }
 export default config
